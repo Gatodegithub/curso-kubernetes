7f9ccb83a770e87d48f500f7858b7e939653f61d
aldo <aldo@gmail.com>
2024-08-26 15:54:16 -0400

    inicio

---
+++ src/main/java/com/asaavedm/springcloud/mvsc/curso_kubernetes/CursoKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.asaavedm.springcloud.mvsc.curso_kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asaavedm/springcloud/mvsc/curso_kubernetes/CursoKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asaavedm.springcloud.mvsc.curso_kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
